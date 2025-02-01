88a2bd61d9fc3dd2fe5fa3ec65dc4dc3f23f8bc8
ahsanrkabir <ahsanrifat001@gamil.com>
2025-02-02 03:34:31 +0600

    Implement Edit Image & Sensitive metadata

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,12 +1,23 @@
 "use client";
 
 import { useState } from "react";
+import NextImage from "next/image";
 
 import Image from "@/components/Image";
+import ImageEditor from "@/components/ImageEditor";
+
 import { shareAction } from "@/actions";
 
 const Share = () => {
   const [media, setMedia] = useState<File | null>(null);
+  const [isEditorOpen, setIsEditorOpen] = useState(false);
+  const [settings, setSettings] = useState<{
+    type: "original" | "wide" | "square";
+    sensitive: boolean;
+  }>({
+    type: "original",
+    sensitive: false,
+  });
 
   const handleMediaChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
@@ -14,8 +25,13 @@ const Share = () => {
     }
   };
 
+  const previewURL = media ? URL.createObjectURL(media) : null;
+
   return (
-    <form action={shareAction} className="p-4 flex gap-4">
+    <form
+      action={(formData) => shareAction(formData, settings)}
+      className="p-4 flex gap-4"
+    >
       {/* USER AVATAR */}
       <div className="relative w-10 h-10 rounded-full overflow-hidden">
         <Image
@@ -29,6 +45,7 @@ const Share = () => {
 
       {/* SHARE INPUT & OTHER OPTIONS */}
       <div className="flex-1 flex flex-col gap-4">
+        {/* TEXT INPUT */}
         <input
           name="desc"
           type="text"
@@ -36,9 +53,44 @@ const Share = () => {
           className="bg-transparent outline-none placeholder:text-textGray text-xl"
         />
 
+        {/* PREVIEW IMAGE */}
+        {previewURL && (
+          <div className="relative rounded-xl overflow-hidden">
+            <NextImage
+              src={previewURL}
+              alt="preview"
+              width={600}
+              height={400}
+              className={`w-full ${
+                settings.type === "original"
+                  ? "h-full object-contain"
+                  : settings.type === "square"
+                  ? "aspect-square object-cover"
+                  : "aspect-video object-cover"
+              }`}
+            />
+            <div
+              onClick={() => setIsEditorOpen(true)}
+              className="absolute top-2 left-2 bg-black bg-opacity-50 text-white py-1 px-4 rounded-full font-bold text-sm cursor-pointer"
+            >
+              Edit
+            </div>
+          </div>
+        )}
+
+        {/* IMAGE EDITOR */}
+        {isEditorOpen && previewURL && (
+          <ImageEditor
+            onClose={() => setIsEditorOpen(false)}
+            previewURL={previewURL}
+            settings={settings}
+            setSettings={setSettings}
+          />
+        )}
+
         <div className="flex items-center justify-between gap-4 flex-wrap">
           <div className="flex gap-4 flex-wrap">
-            {/* IMAGE UPLOAD */}
+            {/* IMAGE INPUT */}
             <input
               name="file"
               type="file"
