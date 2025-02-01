9cc6fd22fb735ed56e264e9f363e9b30528f1aa1
ahsanrkabir <ahsanrifat001@gamil.com>
2025-02-02 02:15:41 +0600

    Implement File Upload in Imagekit

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import { useState } from "react";
+
 import Image from "@/components/Image";
+import { shareAction } from "@/actions";
 
 const Share = () => {
+  const [media, setMedia] = useState<File | null>(null);
+
+  const handleMediaChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setMedia(e.target.files[0]);
+    }
+  };
+
   return (
-    <div className="p-4 flex gap-4">
+    <form action={shareAction} className="p-4 flex gap-4">
       {/* USER AVATAR */}
       <div className="relative w-10 h-10 rounded-full overflow-hidden">
         <Image
@@ -17,6 +30,7 @@ const Share = () => {
       {/* SHARE INPUT & OTHER OPTIONS */}
       <div className="flex-1 flex flex-col gap-4">
         <input
+          name="desc"
           type="text"
           placeholder="What's the word, humming bird?"
           className="bg-transparent outline-none placeholder:text-textGray text-xl"
@@ -24,13 +38,24 @@ const Share = () => {
 
         <div className="flex items-center justify-between gap-4 flex-wrap">
           <div className="flex gap-4 flex-wrap">
-            <Image
-              path="/icons/image.svg"
-              alt=""
-              w={20}
-              h={20}
-              className="cursor-pointer"
+            {/* IMAGE UPLOAD */}
+            <input
+              name="file"
+              type="file"
+              onChange={handleMediaChange}
+              className="hidden"
+              id="file"
             />
+            <label htmlFor="file">
+              <Image
+                path="/icons/image.svg"
+                alt=""
+                w={20}
+                h={20}
+                className="cursor-pointer"
+              />
+            </label>
+
             <Image
               path="/icons/gif.svg"
               alt=""
@@ -73,7 +98,7 @@ const Share = () => {
           </button>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
